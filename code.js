8bb143f9de1b293633c6575134e3c27529e0ccfc
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-11-02 11:00:47 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,47 @@
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    const stack = [{ node: startNode, path: [startNode] }];
+    const visit = new Set();
+
+    while (stack.length > 0) {
+        const { node, path } = stack.pop();
+
+        if (node === targetNode) {
+            return path; // Found the path
+        }
+
+        if (!visit.has(node)) {
+            visit.add(node);
+
+            for (const neighbor of graph[node]) {
+                if (!visit.has(neighbor)) {
+                    stack.push({ node: neighbor, path: path.concat(neighbor) });
+                }
+            }
+        }
+    }
+
+    return []; // No path found
 }
+
+
+const graph = {
+    A: ['B', 'C'],
+    B: ['A', 'D'],
+    C: ['A'],
+    D: ['B'],
+};
+
+const startNode = 'A';
+const targetNode = 'D';
+
+const result = depthFirstSearch(graph, startNode, targetNode);
+
+console.log("Path found:", result);
+
+
+/*
+Sources Used:
+https://www.programiz.com/dsa/graph-dfs
+TA - I was confused about my test implementation and he helped me figue out the test case.
+Referred to Ryan Zafft implementation 
+*/
